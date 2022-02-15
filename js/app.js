a7d7af599e655f997abbe8928a4c7589bd7aca32
lyee197 <liamsyee@gmail.com>
2022-02-15 16:16:17 -0500

    onions

--- js/app.js
+++ js/app.js
@@ -22,18 +22,26 @@ function makeMiddleEarth() {
 	console.log('1: makeMiddleEarth')
 
 	// 1. create a section tag with an id of middle-earth
-
+	const section = document.createElement('section')
+	section.id = 'middle-earth'
 	// 2. use a for loop to iterate over the lands array that does the following:
-
-	//   2a. creates an article tag (there should be one for each land when the loop is done)
-
-	//   2b. gives each land article an `id` tag of the corresponding land name
-
-	//   2c. includes an h1 with the name of the land inside each land article
-
-	//   2d. appends each land to the middle-earth section
-
+	for (let i = 0; i < lands.length; i++) {
+		//   2a. creates an article tag (there should be one for each land when the loop is done)
+		const landArticle = document.createElement('article')
+		//   2b. gives each land article an `id` tag of the corresponding land name
+		landArticle.id = lands[i]
+		//   2c. includes an h1 with the name of the land inside each land article
+		const landName = document.createElement('h1')
+		landName.textContent = lands[i]
+		//   2d. appends each land to the middle-earth section
+		// we need to append the landName to landArticle and append those to middle earth section
+		landArticle.appendChild(landName)
+		section.appendChild(landArticle)
+		
+	}
+	document.body.appendChild(section)
 	// 3. append the section to the body of the DOM.
+
 }
 
 // COMMIT YOUR WORK
@@ -47,11 +55,21 @@ function makeHobbits() {
 	console.log('2: makeHobbits')
 
 	// display an `unordered list` of hobbits in the shire
-
+	// how can I get, 'the shire'? how can i append a ul to it?
+	//I can use querySelector, like this
+	const theShire = document.querySelector('#The-Shire')
+	// console.log('This is the shire' + theShire)
 	// give each hobbit a class of `hobbit`
-
+	const ulHobbits = document.createElement('ul')
+	ulHobbits.id = 'hobbits'
 	// hint: create a 'ul' outside the loop into which to append the 'li's
-
+	for ( let i = 0; i < hobbits.length; i++) {
+		const liHobbit = document.createElement('li')
+		liHobbit.className = 'hobbit'
+		liHobbit.textContnet = hobbits[i]
+		ulHobbits.appendChild(liHobbit)
+	}
+	theShire.appendChild(ulHobbits)
 	// hint: get 'The-Shire' by using its id
 }
 
