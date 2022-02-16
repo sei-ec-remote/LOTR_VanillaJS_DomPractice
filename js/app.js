875649231ff1690db7eeaec2f10b248b021d0a77
patrickga <patrick.g.alvarez@gmail.com>
2022-02-15 19:56:11 -0600

    Chapter 12 complete - Gollum is trying to get the ring

--- js/app.js
+++ js/app.js
@@ -288,8 +288,16 @@ function itsDangerousToGoAlone() {
 function weWantsIt() {
 	console.log('12: weWantsIt')
 	// Create a div with an id of `'gollum'` and add it to Mordor
+	const Mordor = document.getElementById("Mordor")
+	const gollum = document.createElement("div")
+	gollum.id = "gollum"
+	Mordor.appendChild(gollum)
 	// Remove `the ring` from `Frodo` and give it to `Gollum`
+	const oneRing = document.getElementById("the-ring")
+	gollum.appendChild(oneRing)
 	// Move Gollum into Mount Doom
+	const mountDoom = document.getElementById("mount-doom")
+	mountDoom.appendChild(gollum)
 }
 
 // COMMIT YOUR WORK
