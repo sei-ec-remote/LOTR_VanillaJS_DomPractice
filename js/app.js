1fed7881d0be656cbc39ad49bbde6ce494f541b7
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-17 16:28:29 -0400

    makeMiddleEarth function is running

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//test every function you make!!
+
 console.log('Linked.')
 
 // Dramatis Personae
@@ -20,18 +22,25 @@ const lands = ['The-Shire', 'Rivendell', 'Mordor']
 
 function makeMiddleEarth() {
 	console.log('1: makeMiddleEarth')
-
+	
 	// 1. create a section tag with an id of middle-earth
-
+	middleEarth = document.createElement('section');
 	// 2. use a for loop to iterate over the lands array that does the following:
+		
+	for (let i = 0; i< lands.length; i++){
 
-	//   2a. creates an article tag (there should be one for each land when the loop is done)
+		//   2a. creates an article tag (there should be one for each land when the loop is done)
+		land = document.createElement('article');
+		//now you can do something to the element land you just made
 
 	//   2b. gives each land article an `id` tag of the corresponding land name
-
+		landId = document.createElement('id');
 	//   2c. includes an h1 with the name of the land inside each land article
-
+		h1 = document.createElement('h1');
 	//   2d. appends each land to the middle-earth section
+		middleEarth.append(land, landId, h1);
+	}
+	
 
 	// 3. append the section to the body of the DOM.
 }
