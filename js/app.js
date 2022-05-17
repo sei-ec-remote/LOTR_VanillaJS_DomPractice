d7d2b05013389f1c835d1768541597b6361e761f
doorstrawberry <kangminsoo671@gmail.com>
2022-05-17 16:59:30 -0400

    Chapter 10 complete - horn of gandor blew and Boromir is dead

--- js/app.js
+++ js/app.js
@@ -213,8 +213,11 @@ function theBalrog() {
 function hornOfGondor() {
 	console.log('10: hornOfGondor')
 	// pop up an alert that the horn of gondor has been blown
+	alert("Horn of Gondor has been blown")
 	// Boromir's been killed by the Uruk-hai!
 	// Remove `Boromir` from the Fellowship
+	const fellowshipList = document.getElementById('the-fellowship').querySelectorAll('li')
+	fellowshipList[4].remove()
 }
 
 // COMMIT YOUR WORK
