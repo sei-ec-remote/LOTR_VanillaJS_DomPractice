ef0c58f415a5ab9d7455ab08331c719d943e2633
soungbin <soungbin941230@gmail.com>
2022-06-09 13:18:42 -0700

    Chapter 1 complete - Made Middle Earth

--- js/app.js
+++ js/app.js
@@ -22,18 +22,24 @@ function makeMiddleEarth() {
 	console.log('1: makeMiddleEarth')
 
 	// 1. create a section tag with an id of middle-earth
+	let tag1 = document.createElement('section')
+	tag1.setAttribute('id','Middle-Earth')
+	console.log(tag1)
 
 	// 2. use a for loop to iterate over the lands array that does the following:
-
+	for(let i = 0 ; i < lands.length ; i ++){
 	//   2a. creates an article tag (there should be one for each land when the loop is done)
-
+		const landArticle =document.createElement('article')
 	//   2b. gives each land article an `id` tag of the corresponding land name
-
+		landArticle.setAttribute('id', lands[i])
+		console.log(landArticle)
 	//   2c. includes an h1 with the name of the land inside each land article
-
+		landArticle.innerHTML = `<h1> ${lands[i]} </h1>`
 	//   2d. appends each land to the middle-earth section
-
+		tag1.appendChild(landArticle)
+	}
 	// 3. append the section to the body of the DOM.
+	document.querySelector('body').appendChild(tag1)
 }
 
 // COMMIT YOUR WORK
