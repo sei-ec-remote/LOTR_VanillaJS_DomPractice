0ee82afd9bd62f7b10a57734a01b78709f1832bc
shaialoni <gitShai2022>
2022-06-10 08:51:19 -0700

    Chapter 9 complete - Updated Gandalf

--- js/app.js
+++ js/app.js
@@ -203,7 +203,13 @@ function forgeTheFellowShip() {
 function theBalrog() {
 	console.log('9: theBalrog')
 	// change the `'Gandalf'` text to `'Gandalf the White'`
+	const gandalf = document.getElementById('Gandalf the Grey')
+	gandalf.innerText = 'Gandalf the White'
+	gandalf.style.backgroundColor = 'white'
+	gandalf.style.border = 'gray'
+	
 	// apply the following style to the element, make the // background 'white', add a grey border
+	
 }
 
 // COMMIT YOUR WORK
