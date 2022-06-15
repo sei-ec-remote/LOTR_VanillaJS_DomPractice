e7d5f93204f0069b614b126312d1889a9b863537
Zene09 <Zeneorr@gmail.com>
2022-06-14 19:38:21 -0500

    Chapter 2 complete - Made the Hobbits

--- js/app.js
+++ js/app.js
@@ -22,22 +22,22 @@ function makeMiddleEarth() {
 	console.log('1. makeMiddleEarth')
 
 	// 1. create a section tag with an id of middle-earth
-	const middleEarthTag = document.createElement('place')
-	middleEarthTag.id = 'middle-earth'
+	const section = document.createElement('section')
+	section.id = 'middle-earth'
 	// 2. use a for loop to iterate over the lands array that does the following:
 	for (let i = 0; i < lands.length; i++) {
 	//   2a. creates an article tag (there should be one for each land when the loop is done)
-	const articleTag = document.createElement('article')
+	const landsArticle = document.createElement('article')
 	//   2b. gives each land article an `id` tag of the corresponding land name
-	articleTag.id = lands[i]
+	landsArticle.id = lands[i]
 	//   2c. includes an h1 with the name of the land inside each land article
-	const landsArray = document.createElement(`h1`)
+	const landsArray = document.createElement('h1')
 	landsArray.textContent = lands[i]
 	//   2d. appends each land to the middle-earth section
-	middleEarthTag.appendChild(landsArray)
+	section.appendChild(landsArticle)
 	// // 3. append the section to the body of the DOM.
-	document.body.appendChild(middleEarthTag)
 	}
+document.body.appendChild(section)
 }
 
 // COMMIT YOUR WORK
@@ -46,17 +46,22 @@ function makeMiddleEarth() {
 // ============
 // Chapter 2
 // ============
-
 function makeHobbits() {
 	console.log('2: makeHobbits')
-
 	// display an `unordered list` of hobbits in the shire
-	
+	const theShire = document.getElementById('The-Shire')
+	const shireHobbits = document.createElement('ul')
+	shireHobbits.className = 'hobbits'
+
+	for (let i = 0; i < hobbits.length; i++) {
+		const liHobbits = document.createElement('li')
 	// give each hobbit a class of `hobbit`
-
+		liHobbits.className = 'hobbit'
+		shireHobbits.appendChild(liHobbits)
 	// hint: create a 'ul' outside the loop into which to append the 'li's
-
 	// hint: get 'The-Shire' by using its id
+	}
+	theShire.appendChild(shireHobbits)
 }
 
 // COMMIT YOUR WORK
