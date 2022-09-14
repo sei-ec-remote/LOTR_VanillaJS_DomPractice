159835469a9d0a531a2f77300cfc073c650d2fd6
angie10 <delacruzvargasangelica26@gmail.com>
2022-09-14 19:26:15 -0400

    Chapter 3 complete - Made the ring and gave it to Frodo

--- js/app.js
+++ js/app.js
@@ -84,10 +84,12 @@ function keepItSecretKeepItSafe() {
 	console.log('3: keepItSecretKeepItSafe')
 
 	// create a div with an id of `'the-ring'`
-
+      const div = document.createElement('div');
+	  div.setAttribute('id', 'the-ring');
 	// give the div a class of `'magic-imbued-jewelry'`
-
+     div.setAttribute('class', 'magic-imbued-jewelry');
 	// add the ring as a child of `Frodo`
+      frodo.appendChild('div');
 }
 
 // COMMIT YOUR WORK
