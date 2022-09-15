0175e3c5728e267ebdf1de2783e126a8034f02b8
JChenoweth <jchenoweth@Jennifers-MBP.local>
2022-09-14 21:58:54 -0500

    Chapter 5 complete - Added the buddies.

--- js/app.js
+++ js/app.js
@@ -141,7 +141,17 @@ function makeBuddies() {
 	document.createElement('aside')
 
 	// put an `unordered list` of the `'buddies'` in the aside
+	const ul = document.createElement("ul")
 
+	for(let i = 0; i < buddies.length; i++) {
+		let li = document.createElement("li")
+		li.setAttribute("class", "hobbit")
+		li.setAttribute('id', 'buddies' + [i])
+		ul.appendChild(li)
+		li.innerText = buddies[i]
+	}
+
+	document.getElementById(lands[1]).appendChild(ul)
 	// insert your aside as a child element of `rivendell`
 }
 
