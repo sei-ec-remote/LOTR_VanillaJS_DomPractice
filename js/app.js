3a6c66703ad53938e80c3efd7b96ceab16b52055
isobelmorales <isofmorales@gmail.com>
2022-12-20 11:24:03 -0500

    Chapter 5 complete - Made the Buddies

--- js/app.js
+++ js/app.js
@@ -163,10 +163,23 @@ function makeBuddies() {
 	console.log('5: makeBuddies')
 
 	// create an `aside` tag
+    const aside = document.createElement('aside')
+    aside.id = 'buddies-aside'
 
 	// put an `unordered list` of the `'buddies'` in the aside
+    const ulBuddies = document.createElement('ul')
+    ulBuddies.id = 'buddies'
+    for (let i = 0; i < buddies.length; i++) {
+        const liBuddy = document.createElement('li')
+        liBuddy.className = 'buddy'
+        liBuddy.textContent = buddies[i]
+        ulBuddies.appendChild(liBuddy)
+    }
+    aside.appendChild(ulBuddies)
 
 	// insert your aside as a child element of `rivendell`
+    const rivendell = document.getElementById('Rivendell')
+    rivendell.appendChild(aside)
 }
 
 // COMMIT YOUR WORK
